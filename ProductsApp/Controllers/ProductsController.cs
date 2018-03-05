e24535bafbcbdbdf82d0516e96a6d68e57df89b5
javierlete <jglete@gmail.com>
2018-03-05 14:28:59 +0100

    Versión inicial de Web API de productos con /api/products y /api/products/{id}

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        // /api/products
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        // /api/products/{id}
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
